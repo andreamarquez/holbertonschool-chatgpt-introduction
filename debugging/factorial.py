56a1c01a5ad06cbcdc65aef5127ef06cb43e5437
andreamarquez <andreamarquezg.95@gmail.com>
2025-01-13 09:23:14 +0100

    error handling

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -8,5 +8,17 @@ def factorial(n):
         n -= 1  # Decrement n
     return result
 
-f = factorial(int(sys.argv[1]))
+if len(sys.argv) != 2:
+    print("Usage: ./factorial.py <number>")
+    sys.exit(1)
+
+try:
+    n = int(sys.argv[1])
+    if n < 0:
+        raise ValueError("Number must be non-negative")
+except ValueError as e:
+    print(e)
+    sys.exit(1)
+
+f = factorial(n)
 print(f)
