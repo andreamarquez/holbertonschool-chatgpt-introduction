9ace8991b91328e86865ebb3fdbb8a9034ca59bd
andreamarquez <andreamarquezg.95@gmail.com>
2025-01-13 15:01:03 +0100

    code updated

--- debugging/mines.py
+++ debugging/mines.py
@@ -1,65 +1,54 @@
 #!/usr/bin/env python3
 
 import random
+import os
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 class Minesweeper:
     def __init__(self, width=10, height=10, mines=10):
         self.width = width
         self.height = height
-        self.mines = mines
-        self.board = [[0 for _ in range(width)] for _ in range(height)]
+        self.mines = set(random.sample(range(width * height), mines))
+        self.field = [[' ' for _ in range(width)] for _ in range(height)]
         self.revealed = [[False for _ in range(width)] for _ in range(height)]
-        self.place_mines()
-        self.calculate_numbers()
         self.non_mine_cells = width * height - mines
 
-    def place_mines(self):
-        mines_placed = 0
-        while mines_placed < self.mines:
-            x = random.randint(0, self.width - 1)
-            y = random.randint(0, self.height - 1)
-            if self.board[y][x] != -1:
-                self.board[y][x] = -1
-                mines_placed += 1
-
-    def calculate_numbers(self):
+    def print_board(self, reveal=False):
+        clear_screen()
+        print('  ' + ' '.join(str(i) for i in range(self.width)))
         for y in range(self.height):
+            print(y, end=' ')
             for x in range(self.width):
-                if self.board[y][x] == -1:
-                    continue
-                self.board[y][x] = self.count_mines_nearby(x, y)
+                if reveal or self.revealed[y][x]:
+                    if (y * self.width + x) in self.mines:
+                        print('*', end=' ')
+                    else:
+                        count = self.count_mines_nearby(x, y)
+                        print(count if count > 0 else ' ', end=' ')
+                else:
+                    print('.', end=' ')
+            print()
 
     def count_mines_nearby(self, x, y):
         count = 0
         for dx in [-1, 0, 1]:
             for dy in [-1, 0, 1]:
                 nx, ny = x + dx, y + dy
-                if 0 <= nx < self.width and 0 <= ny < self.height and self.board[ny][nx] == -1:
-                    count += 1
+                if 0 <= nx < self.width and 0 <= ny < self.height:
+                    if (ny * self.width + nx) in self.mines:
+                        count += 1
         return count
 
-    def print_board(self, reveal=False):
-        for y in range(self.height):
-            row = ""
-            for x in range(self.width):
-                if reveal or self.revealed[y][x]:
-                    if self.board[y][x] == -1:
-                        row += "* "
-                    else:
-                        row += f"{self.board[y][x]} "
-                else:
-                    row += ". "
-            print(row)
-        print()
-
     def reveal(self, x, y):
+        if (y * self.width + x) in self.mines:
+            return False
         if self.revealed[y][x]:
             return True
-        if self.board[y][x] == -1:
-            return False
         self.revealed[y][x] = True
         self.non_mine_cells -= 1
-        if self.board[y][x] == 0:
+        if self.count_mines_nearby(x, y) == 0:
             for dx in [-1, 0, 1]:
                 for dy in [-1, 0, 1]:
                     nx, ny = x + dx, y + dy
